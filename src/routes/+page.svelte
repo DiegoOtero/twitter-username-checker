d94a4e271b315e324aee573da2dc749b4cd21d97
Diego Otero <13272809+DiegoOtero@users.noreply.github.com>
2023-05-11 15:52:21 +0200

    Create basic design Fixes #6
    Create basic design of the app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="flex">
+		<div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+			<div
+				class="relative isolate overflow-hidden bg-gray-50 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0"
+			>
+				<div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-14 lg:text-left mb-7">
+					<div class="flex justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-10 w-10"
+							fill="currentColor"
+							style="color: #1da1f2"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+							/>
+						</svg>
+					</div>
+					<div class="flex justify-center mt-8">
+						<h1 class="font-bold tracking-tight text-gray-900 sm:text-4xl">
+							Twitter Username Checker
+						</h1>
+					</div>
+					<div class="flex justify-center mt-8">
+						<div class="flex mt-2 rounded-md shadow-sm">
+							<span
+								class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-100 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+							>
+								@
+							</span>
+							<input
+								type="text"
+								id="username"
+								class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="Username"
+								required
+							/>
+						</div>
+					</div>
+					<div class="flex justify-center mt-7 gap-x-6">
+						<button
+							type="submit"
+							class="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-400"
+							>Check</button
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
